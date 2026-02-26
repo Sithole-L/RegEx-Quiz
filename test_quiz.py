f594ae04dcc528fdbe3d70531e8883f20ce9a87d
Lawrence Sithole <lawrence.sithole@defra.gov.uk>
2026-02-26 15:28:25 +0000

    smoke test

---
+++ test_quiz.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestRegexQuiz(unittest.TestCase):
+
+    def test_smoke(self):
+        """
+        Simple smoke test to ensure the system runs.
+        """
+        self.assertTrue(True)
+
+
+
+if __name__ == "__main__":
+    unittest.main()

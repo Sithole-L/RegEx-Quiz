7317ffb4ade1693de386192b0fe28c443474cf03
Lawrence Sithole <lawrence.sithole@defra.gov.uk>
2026-02-26 15:57:51 +0000

    csv and more tests

--- test_quiz.py
+++ test_quiz.py
@@ -1,4 +1,6 @@
 import unittest
+import os
+import csv
 
 
 class TestRegexQuiz(unittest.TestCase):
@@ -8,6 +10,26 @@ class TestRegexQuiz(unittest.TestCase):
         Simple smoke test to ensure the system runs.
         """
         self.assertTrue(True)
+
+    def setUp(self):
+        """
+        Creates a temporary CSV for testing engine logic
+        """
+        self.test_file = 'test_questions.csv'
+        self.test_results_file = 'test_results.csv'
+
+        with open(self.test_file, 'w', newline='', encoding='utf-8') as f:
+            writer = csv.writer(f)
+            writer.writerow(["prompt","o1","o2","o3","o4","answer"])  
+            writer.writerow(["test q", "a","b","c","d","a"])
+
+    def tearDown(self):
+        """
+        Cleans up all temporary files after tests run.
+        """
+        for file in [self.test_file, self.test_results_file]:
+            if os.path.exists(file):
+                os.remove(file)      
 
 
 
